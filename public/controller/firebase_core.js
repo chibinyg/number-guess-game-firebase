eb5bc93eb9ed079d127dd58af7fbea228cc26c0e
chibin yang <chibinyg@yahoo.com>
2025-01-26 16:08:26 -0600

    init commit

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBbEybY590DA4XQJFPrUPEZnVwsXjlCKCs",
+    authDomain: "cyang19-cmsc4373.firebaseapp.com",
+    projectId: "cyang19-cmsc4373",
+    storageBucket: "cyang19-cmsc4373.firebasestorage.app",
+    messagingSenderId: "767093749785",
+    appId: "1:767093749785:web:c905e2d4319092312e1288"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
